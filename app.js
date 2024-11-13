3335385a203669faf0874a5df297f6f6afde7f31
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-11-13 23:51:48 +0530

    ES Module Error Fix

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ import jwt from "./plugins/jwt.js"
 import auth from './routes/auth.js'
 import thumbnail from './routes/thumbnail.js'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 configDotenv()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fastify = Fastify({ logger: true })
 
